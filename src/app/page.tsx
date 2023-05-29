93458a34d809caa9728061ff41bd5dcb0df6bdb0
Vinicius Goncalves <viniciusfod@gmail.com>
2023-05-29 18:19:51 -0300

    feat: earth model3d

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,52 @@
 'use client';
 
 import { Canvas } from "@react-three/fiber";
-import { OrbitControls } from "@react-three/drei";
+import { OrbitControls, Center } from "@react-three/drei";
 import { Terra } from "./components/Terra";
 import { Suspense } from "react";
+import styled from "styled-components";
+import React, { useState } from "react";
+
+interface WrapperProps {
+  grabbing: boolean;
+}
+
+const Wrapper = styled.div<WrapperProps>`
+  position: relative;
+  background-color: #1f1144;
+
+  canvas {
+    height: 100vh;
+    cursor: ${props => (props.grabbing ? 'grabbing' : 'grab')};
+  }
+`;
 
 export default function Home() {
+  const [isGrabbing, setIsGrabbing] = useState<boolean>(false);
+
+  const handlePageClick = () => {
+    setIsGrabbing(!isGrabbing);
+  };
+
   return (
-    <main>
-      aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
-      <Canvas>
-        <OrbitControls enableZoom={true}/>
-        <ambientLight />
-        <pointLight position={[10, 10, 10]} />
-        <Suspense>
-          <Terra />
+    <Wrapper grabbing={isGrabbing}>
+      <Canvas
+        camera={{ position: [30, 10, 10], fov: 60 }}
+        className={'canvas'}
+        onPointerDown={handlePageClick}
+        onPointerUp={handlePageClick}
+      >
+        <Suspense fallback={null}>
+          <OrbitControls
+            enableRotate={true}
+            enablePan={false}
+            enableZoom={false}
+          />
+          <Center>
+            <Terra />
+          </Center>
         </Suspense>
       </Canvas>
-    </main>
+    </Wrapper>
   );
 }
