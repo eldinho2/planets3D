713a3e15904f2758f45c14a32c9ec7abf5d19061
Vinicius Goncalves <viniciusfod@gmail.com>
2023-05-30 13:35:11 -0300

    test: fps test mobile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
 'use client';
 
 import { Canvas } from "@react-three/fiber";
-import { OrbitControls, Center } from "@react-three/drei";
+import { OrbitControls, Center, Sparkles, useProgress, Html} from "@react-three/drei";
+import { Perf } from "r3f-perf";
 import { Terra } from "./components/Terra";
 import { Lua } from "./components/Lua";
 import { Suspense } from "react";
@@ -14,6 +15,8 @@ interface WrapperProps {
 
 const Wrapper = styled.div<WrapperProps>`
   position: relative;
+  background-color: #000;
+  color: #fff;
 
   canvas {
     height: 400px;
@@ -22,6 +25,10 @@ const Wrapper = styled.div<WrapperProps>`
   }
 `;
 
+function Loader() {
+  const { progress} = useProgress()
+  return <Html center>{progress.toFixed(2)} % loaded</Html>
+}
 
 export default function Home() {
   const [isGrabbing, setIsGrabbing] = useState<boolean>(false);
@@ -38,35 +45,42 @@ export default function Home() {
         onPointerDown={handlePageClick}
         onPointerUp={handlePageClick}
       >
-        <Suspense fallback={null}>
+        <Suspense fallback={<Loader />}>
           <OrbitControls
-            enableRotate={true}
+            enableRotate={false}
             enablePan={false}
             enableZoom={false}
+            autoRotate autoRotateSpeed={0.3}
           />
-                    <ambientLight intensity={0.2} />
-
-          <Center>
-            <Terra />
-          </Center>
+          <ambientLight intensity={0.2} />
+          <Perf position="top-left" />
+          <Sparkles count={1000} scale={1111 * 2} size={1} speed={0.4}>
+            <Center>
+              <Terra />
+            </Center>
+          </Sparkles>
         </Suspense>
       </Canvas>
       <Canvas
-        camera={{ position: [30, 10, 10], fov: 60 }}
+        camera={{ position: [2, 2, 2], fov: 60 }}
         className={'canvas'}
         onPointerDown={handlePageClick}
         onPointerUp={handlePageClick}
       >
-        <Suspense fallback={null}>
+        <Suspense fallback={<Loader />}>
           <OrbitControls
-            enableRotate={true}
-            enablePan={true}
-            enableZoom={true}
+            enableRotate={false}
+            enablePan={false}
+            enableZoom={false}
+            autoRotate autoRotateSpeed={0.3}
           />
           <ambientLight intensity={0.2} />
+          <Perf position="bottom-left" />
+          <Sparkles count={1000} scale={1111 * 2} size={1} speed={0.4}>
           <Center>
             <Lua />
           </Center>
+          </ Sparkles>
         </Suspense>
       </Canvas>
     </Wrapper>
